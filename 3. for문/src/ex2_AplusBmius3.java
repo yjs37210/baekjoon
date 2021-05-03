37e3116c3f5e81a44518518eac1ab78c7043468e
ej <lliac@naver.com>
2021-05-03 21:36:18 +0900

    3_ex2

---
+++ 3. for문/src/ex2_AplusBmius3.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex2_AplusBmius3 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int case_num = Integer.parseInt(br.readLine());
+		
+		String[] strs;
+		int[][] nums = new int[case_num][2]; 
+ 		for(int i = 0; i < case_num; i++) {
+ 			strs = br.readLine().split(" ");
+ 			nums[i][0] = Integer.parseInt(strs[0]);
+ 			nums[i][1] = Integer.parseInt(strs[1]);
+		}
+ 		
+ 		sum(nums);
+ 		
+	}
+
+	private static void sum(int[][] nums) {
+
+		for(int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i][0] + nums[i][1]);
+		}
+		
+	}
+
+}
