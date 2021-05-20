2aef16aec06c7e7bc5fe0968ca443007eabaeef4
ej <lliac@naver.com>
2021-05-21 08:51:10 +0900

    20210520

---
+++ 8. 기본 수학 1/src/ex5_ACMhotel.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class ex5_ACMhotel {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+		
+		String[] strs;
+		for(int i = 0; i < T; i++) {
+			
+			strs = br.readLine().split(" ");
+			int H = Integer.parseInt(strs[0]);
+			int N = Integer.parseInt(strs[2]);
+			
+			bw.write(room(H, N) + "\n");
+			
+		}
+
+		bw.close();
+	}
+
+	private static String room(int H, int N) {
+
+		int Y = 0;
+		int X = 0;
+		
+		if(N % H == 0) {
+			Y = H;
+			X = N / H;
+		}else {
+			Y = N % H;
+			X = N / H + 1;
+		}
+		
+		if(X < 10) {
+			return Y + "0" + X;
+		}
+		
+		return Y + "" + X;
+	}
+
+}
